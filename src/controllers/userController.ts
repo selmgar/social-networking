66a60425070d6c3d71f963c99a49ad2ed8a80d5b
@selmgar-eEHSZr <selmgar@gmail.com>
2024-10-08 22:44:32 -0500

    Creates an endpoint for getting a single user. The thoughts and friends are only populated if there are thoughts and friends IDs. Need to test

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -21,3 +21,26 @@ export const getUsers = async (_req: Request, res: Response) => {
       res.status(500).json(err);
     }
   }
+
+  // Get a single user by their ID
+  export const getUserById = async (req: Request, res: Response) => {
+    try {
+      const user = await User.findOne({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      // TODO ensure this is working once we have thoughts and friends
+      if (user.thoughts.length > 0) {
+        await user.populate('thoughts');
+      }
+      if (user.friends.length > 0) {
+        await user.populate('friends');
+      }
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { createUser, getUsers } from '../controllers/userController.js';
+import { createUser, getUserById, getUsers } from '../controllers/userController.js';
 
 // Create a new router
 const router = Router();
 
 // GET & POST route for /api/users
 router.route('/').get(getUsers).post(createUser);
+
+// GET single user
+router.route('/:userId').get(getUserById)
 
 export default router;
